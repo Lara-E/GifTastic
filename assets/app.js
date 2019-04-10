fe918a688bbd4437e782f7e1db6caad1c86274ca
Lara Eller <lareh_eller@yahoo.com>
2019-04-10 01:54:16 -0400

    I added css

--- assets/app.js
+++ assets/app.js
@@ -9,28 +9,29 @@ $("document").ready(function () {
 
     var createButtons = function () {
         $("#button-display").empty();
+        $("#holiday-input").val("");
         for (var i = 0; i < holidays.length; i++) {
             var holidaysBtn = $("<button>");
             holidaysBtn.addClass("button");
             holidaysBtn.attr("data-holiday", holidays[i]);
             holidaysBtn.html(holidays[i]);
             $("#button-display").append(holidaysBtn);
-            // display();
         }
     }
 
     $("#add-holiday").on("click", function (event) {
         event.preventDefault();
         var holidayInput = $("#holiday-input").val().trim();
-        if (holidayInput !== "")
+        if (holidayInput !== ""){
         holidays.push(holidayInput);
         createButtons();
+        }
     });
 
     createButtons();
 
-    // var display = function() {
-        $(".button").on("click", function () {
+
+        $(document).on("click", "button", function() {
             $("img").remove();
             $("p").remove();
             console.log($(this).attr("data-holiday"))
@@ -44,19 +45,19 @@ $("document").ready(function () {
                 var results = response.data;
 
                 for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div>");
+                    var gifDiv = $("<div class='table'>");
                     var rating = results[i].rating;
-                    var p = $("<p>").text("Rating: " + rating);
+                    var p = $("<p>").text("Rating: " + rating.toUpperCase());
                     var holidayImageStill = results[i].images.fixed_height_still.url;
                     var holidayImageAnimate = results[i].images.fixed_height.url;
                     var imageDisplay = $("<img>");
                     imageDisplay.addClass("image").attr({ "src": holidayImageStill, "data-url": holidayImageAnimate });
-                    gifDiv.append(p, imageDisplay);
+                    gifDiv.append(imageDisplay, p);
 
                     $("#gif-display").prepend(gifDiv);
                 }
 
-                $(".image").on("click", function () {
+                $(".image").on("click", function() {
                     console.log($(this).attr("data-url"))
                     var still = $(this).attr("src");
                     var animate = $(this).attr("data-url");
